3d30fba06fa0b0fd33cbbd2cdcbcd890ca7ff23b
xxs <550419038@qq.com>
2024-10-12 13:56:04 +0800

    3.16

--- build.sh
+++ build.sh
@@ -10,3 +10,4 @@ docker-compose build jieba-ext
 docker run --name jieba${PHP_VERSION}-v${ALPINE_VERSION} hyperf/jieba:${PHP_VERSION}-v${ALPINE_VERSION}
 docker cp jieba${PHP_VERSION}-v${ALPINE_VERSION}:/opt/www/modules/jieba.so build/jieba-${PHP_VERSION}-alpine-v${ALPINE_VERSION}.so
 docker rm jieba${PHP_VERSION}-v${ALPINE_VERSION}
+
